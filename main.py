4fe638a6fd24e4f7d9c74d392dda63a8517226d1
le6790 <le6790@gmail.com>
2022-05-12 15:59:45 -0600

    Added a function get_single_post to return specific data on a post

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import PrawReddit
 def main():
 
     reddit_read_only = PrawReddit.PrawReddit(config.CONFIG)
+    #reddit_read_only.get_hot_posts("formula1",10)
 
+    post_dict = reddit_read_only.get_single_post("uo0v00")
 
-    subreddit = reddit_read_only.get_subreddit("redditdev")
+    print(reddit_read_only.post_to_json(post_dict))
+    reddit_read_only.write_post_to_json_file(post_dict,)
 
-    print(subreddit.display_name)
-
-    reddit_read_only.get_hot_posts("formula1",10)
 
 if __name__ == '__main__':
     main()

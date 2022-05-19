06b276295ee55f598c29a83739caba2cf0d31eb7
le6790 <le6790@gmail.com>
2022-05-19 17:21:11 -0600

    Dirty, but working

--- create_video.py
+++ create_video.py
@@ -63,48 +63,50 @@ class Create_Video:
 
     def create_single_comment_clip(self, text, author, score, mp3_file,):
         # Create the clip
-
+        audio_clip = mpye.AudioFileClip(mp3_file)
         ### Text Clip ###
         text_clip = mpye.TextClip(txt=text,
-                                   fontsize=40,
-                                   color="black",
+                                   fontsize=30,
+                                   color="white",
                                    method="caption",
                                    size=(750, 0),
                                     stroke_color="white",
-                                    stroke_width=3
+                                    stroke_width=2
                                    )
-        text_clip = text_clip.set_position(("center"))
+        text_clip = text_clip.set_position(("center",100))
         text_clip_width, text_clip_height = text_clip.size
 
         ### END Text Clip ###
 
         ### Author Clip ###
-        author_clip = mpye.TextClip(txt=f"u/{author}",
-                                   fontsize=30,
-                                   color="black",
+        author_clip = mpye.TextClip(txt=f"Posted by u/{author}",
+                                   fontsize=25,
+                                   color="gray",
                                    method="caption",
-                                   size=(0,50),
-                                    stroke_color="white",
-                                    stroke_width=3
+                                   size=(800,0),
+                                    stroke_color="rgb(217, 217, 217)",
+                                    stroke_width=1,
+                                    align="west"
                                    )
-        author_clip = author_clip.set_position(25,50)
+        author_clip = author_clip.set_position((25,20))
         ### End Author Clip ###
 
-        ### Score Clip ###
-        score_clip = mpye.TextClip(txt=f"{score}",
-                                   fontsize=30,
-                                   color="black",
-                                   method="caption",
-                                   size=(0,50),
-                                    stroke_color="white",
-                                    stroke_width=3
-                                   )
-        score_clip = score_clip.set_position(25,100)
-
+        # ### Score Clip ###
+        # score_clip = mpye.TextClip(txt=f"{score}",
+        #                            fontsize=30,
+        #                            color="white",
+        #                            method="caption",
+        #                            size=(0,50),
+        #                             stroke_color="white",
+        #                             stroke_width=1
+        #                            )
+        # score_clip = score_clip.set_position((50,50))
+        score_clip = self.create_score_clip(score,audio_clip.duration)
+        score_clip = score_clip.set_position((25,30))
         # Create the clip background
         # colored background
         clip_color_background = mpye.ColorClip(size=(text_clip_width+100,
-                                                   text_clip_height+50),
+                                                   text_clip_height+150),
                                              color=(109, 109, 109))
         clip_color_background = clip_color_background.set_opacity(0.6)
     
@@ -112,7 +114,7 @@ class Create_Video:
         final_clip = mpye.CompositeVideoClip([clip_color_background, author_clip, score_clip, text_clip])
 
         #set the final clip audio and the clip duration to the audio's duration
-        audio_clip = mpye.AudioFileClip(mp3_file)
+        
 
         final_clip = final_clip.set_duration(audio_clip.duration)
 
@@ -120,16 +122,50 @@ class Create_Video:
 
         return final_clip
 
+    def create_score_clip(self,score, duration):
+
+        upvote_arrow = mpye.ImageClip("resources/upvote.png")
+        upvote_arrow = upvote_arrow.resize(0.035)
+        upvote_arrow = upvote_arrow.set_position(("left","center"))
+        downvote_arrow =  mpye.ImageClip("resources/downvote.png")
+        downvote_arrow = downvote_arrow.resize(0.035)
+        downvote_arrow = downvote_arrow.set_position("right","center")
+        
+        
+        
+        score_clip = mpye.TextClip(txt=f"{score}",
+                                   font="Helvetica",
+                                   fontsize=26,
+                                   color="white",
+                                   method="caption",
+                                   size=(0,50),
+                                    stroke_color="white",
+                                    stroke_width=1
+                                   )
+        score_clip = score_clip.set_position(("center"))
+
+        score_clip_width, score_clip_height = score_clip.size
+
+        background = mpye.ColorClip(size=(score_clip_width + 50,score_clip_height + 5),color=(255,0,0))
+        background = background.set_opacity(0.0)
+
+        final_score_clip = mpye.CompositeVideoClip([background,upvote_arrow,score_clip,downvote_arrow])
+        final_score_clip = final_score_clip.set_duration(duration)
+
+        return final_score_clip
     def create_comment_clips(self):
         comments = self.post_dict["comments"]
 
         comment_clips = []
         for comment in comments[:3]: #TODO: loop over comments a different way
             actual_comment = comment["comment"]
+            author = comment["author"]
+            score = comment["score"]
             comment_mp3_filename = f"comment_{comment['score']}_{comment['id']}.mp3"
             comment_mp3_filepath = self.get_files(comment_mp3_filename)
 
-            comment_clip = self.create_clip_with_background(actual_comment, comment_mp3_filepath)
+            # comment_clip = self.create_clip_with_background(actual_comment, comment_mp3_filepath)
+            comment_clip = self.create_single_comment_clip(actual_comment,author, score, comment_mp3_filepath)
 
             comment_clips.append(comment_clip)
 

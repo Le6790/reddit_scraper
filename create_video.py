1e1adea57c8f149017aa2cbce256cc65b5cb4890
le6790 <le6790@gmail.com>
2022-05-19 23:54:52 -0600

    completed formatting of comment boxes. First video up on Tiktok

--- create_video.py
+++ create_video.py
@@ -23,15 +23,13 @@ class Create_Video:
 
         title_text = self.post_dict["title"]
         title_mp3_file = self.get_files("title.mp3")
-
-        final_title_clip = self.create_clip_with_background(title_text, title_mp3_file)
-
-        return final_title_clip
-
-    def create_clip_with_background(self, text, mp3_file):
+        score = self.post_dict["score"]
+        
+        #set the final clip audio and the clip duration to the audio's duration
+        audio_clip = mpye.AudioFileClip(title_mp3_file)
 
         # Create the clip
-        clip = mpye.TextClip(txt=text,
+        clip = mpye.TextClip(txt=title_text,
                                    fontsize=40,
                                    color="black",
                                    method="caption",
@@ -42,6 +40,13 @@ class Create_Video:
         clip = clip.set_position(("center"))
         clip_width, clip_height = clip.size
 
+        # ### Score Clip ###
+        score_clip = self.create_title_score_clip(score,audio_clip.duration)
+        score_clip = score_clip.set_position((10,10))
+        ### END Score Clip ###
+
+
+
         # Create the clip background
         # colored background
         clip_color_background = mpye.ColorClip(size=(clip_width+100,
@@ -50,16 +55,17 @@ class Create_Video:
         clip_color_background = clip_color_background.set_opacity(0.6)
     
         #compose the final clip + background
-        final_clip = mpye.CompositeVideoClip([clip_color_background, clip])
+        final_clip = mpye.CompositeVideoClip([clip_color_background, score_clip, clip])
 
-        #set the final clip audio and the clip duration to the audio's duration
-        audio_clip = mpye.AudioFileClip(mp3_file)
+        
 
         final_clip = final_clip.set_duration(audio_clip.duration)
 
         final_clip.audio = audio_clip
 
+        self.title_clip = final_clip
         return final_clip
+
 
     def create_single_comment_clip(self, text, author, score, mp3_file,):
         # Create the clip
@@ -92,22 +98,16 @@ class Create_Video:
         ### End Author Clip ###
 
         # ### Score Clip ###
-        # score_clip = mpye.TextClip(txt=f"{score}",
-        #                            fontsize=30,
-        #                            color="white",
-        #                            method="caption",
-        #                            size=(0,50),
-        #                             stroke_color="white",
-        #                             stroke_width=1
-        #                            )
-        # score_clip = score_clip.set_position((50,50))
         score_clip = self.create_score_clip(score,audio_clip.duration)
         score_clip = score_clip.set_position((25,30))
+        ### END Score Clip ###
+
+
         # Create the clip background
         # colored background
         clip_color_background = mpye.ColorClip(size=(text_clip_width+100,
                                                    text_clip_height+150),
-                                             color=(109, 109, 109))
+                                                   color=(109, 109, 109))
         clip_color_background = clip_color_background.set_opacity(0.6)
     
         #compose the final clip + background
@@ -153,11 +153,46 @@ class Create_Video:
         final_score_clip = final_score_clip.set_duration(duration)
 
         return final_score_clip
+
+
+    def create_title_score_clip(self, score, duration):
+        upvote_arrow = mpye.ImageClip("resources/upvote.png")
+        upvote_arrow = upvote_arrow.resize(0.035)
+        upvote_arrow = upvote_arrow.set_position(("center","top"))
+        downvote_arrow =  mpye.ImageClip("resources/downvote.png")
+        downvote_arrow = downvote_arrow.resize(0.035)
+        downvote_arrow = downvote_arrow.set_position("center","bottom")
+
+
+        score_clip = mpye.TextClip(txt=f"{score}",
+                                   font="Helvetica",
+                                   fontsize=26,
+                                   color="white",
+                                   method="caption",
+                                   size=(0,100),
+                                    stroke_color="white",
+                                    stroke_width=1
+                                   )
+        score_clip = score_clip.set_position(("center",0))
+
+        score_clip_width, score_clip_height = score_clip.size
+
+        background = mpye.ColorClip(size=(score_clip_width + 5,score_clip_height + 60),color=(255,0,0))
+        background = background.set_opacity(0.0)
+
+        final_score_clip = mpye.CompositeVideoClip([background,upvote_arrow,score_clip,downvote_arrow])
+        final_score_clip = final_score_clip.set_duration(duration)
+
+
+        return final_score_clip
+
+
+
     def create_comment_clips(self):
         comments = self.post_dict["comments"]
 
         comment_clips = []
-        for comment in comments[:3]: #TODO: loop over comments a different way
+        for comment in comments[:4]: #TODO: loop over comments a different way
             actual_comment = comment["comment"]
             author = comment["author"]
             score = comment["score"]
@@ -169,28 +204,9 @@ class Create_Video:
 
             comment_clips.append(comment_clip)
 
-        # #Check if clip's height is greater than a certain value, if so, exclude that clip
-        # width = -1
-        # height = -1
-        # for clip in comment_clips:
-        #     clip_width, clip_height = clip.size
-        #     print(f"clip_width: {clip_width}")
-        #     print(f"clip_height: {clip_height}")
-        #     print("-----")
-        #     if clip_height > height:
-        #         height = clip_height
-        #         width = clip_width
-        # print(f"width: {width}")
-        # print(f"height: {height}")
-        # new_comment_clips = []
-        # if width !=-1 and height != -1:
-        #     for clip in comment_clips:
-        #         clip.resize((width,height))
+        title_clip = self.title_clip
 
-        #         new_comment_clips.append(clip)
-            
-
-
+        comment_clips.insert(0,title_clip)
         concatenated_comment_clips = mpye.concatenate_videoclips(comment_clips)
         return concatenated_comment_clips
 
@@ -207,7 +223,7 @@ class Create_Video:
         background_clip = background_clip.set_duration(clips.duration)
 
         final_clip = mpye.CompositeVideoClip([background_clip,clips.set_position("center")])
-        final_clip.write_videofile(name,fps=24)
+        final_clip.write_videofile(name)
         print(f"Saved clip as a video - {name}")
 
     def get_files(self, file):
